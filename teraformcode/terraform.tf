17060c71c89f7e4f33415a49e73703391e83999f
dounmogni <dounmogni@yahoo.fr>
2023-07-26 14:46:25 +0200

    init

---
+++ teraformcode/terraform.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-05548f9cecf47b442"
+    instance_type = "t2.micro"
+    key_name = "devops-nicolas"
+}
